24a91f326d6798d8c49f9c8495f65d421bd4dd56
Nicole Witherall <nicwitherall@gmail.com>
2022-09-04 16:50:49 -0400

    initialize

---
+++ hardlyfrontend/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hardly Ceramics</title>
+        <meta name="description" content="A project by Nicole Witherall" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>hardly ready yet</h1>
+      </main>
+    </div>
+  );
+}
